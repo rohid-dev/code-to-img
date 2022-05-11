6658e91adcaac5147a6f1b55bac8609c86b1056a
Rohidul <rohidul209@gmail.com>
2022-05-11 22:24:12 +0600

    Github Repo Link added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { FaCog, FaGithub } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 const Header = () => {
   return (
     <div className="w-full h-14 flex items-center px-4 gap-2">
@@ -16,9 +16,13 @@ const Header = () => {
       {/* <button className="w-10 h-10 flex items-center justify-center hover:bg-white/10 active:bg-white/20 rounded-full">
         <FaCog className="text-2xl" />
       </button> */}
-      <button className="w-10 h-10 flex items-center justify-center hover:bg-white/10 active:bg-white/20 rounded-full">
+      <a
+        href="https://github.com/rohidisdev/code-to-img"
+        target="_blank"
+        className="w-10 h-10 flex items-center justify-center hover:bg-white/10 active:bg-white/20 rounded-full"
+      >
         <FaGithub className="text-2xl" />
-      </button>
+      </a>
     </div>
   );
 };
