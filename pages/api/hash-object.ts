5de0e75380160d288157282ffd557ade2bcd3bdd
Rohidul <rohidul209@gmail.com>
2022-05-11 23:40:43 +0600

    Secret key moved to env

--- pages/api/hash-object.ts
+++ pages/api/hash-object.ts
@@ -5,23 +5,16 @@ export default async function hanlde(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const key = "asldfj;asdjfkljasdl;fj";
+  const key = process.env.SECRET_KEY;
   if (req.method === "POST") {
-    console.log(req.body);
     const token = await jwt.sign(req.body, key);
-    console.log({ hashed: token });
-    const d = await jwt.verify(token, key);
-    console.log({ d });
-
     res.json({ token });
     return;
   }
 
   if (req.method === "GET") {
     const { token } = req.query;
-    console.log({ token });
     const data = await jwt.verify(token, key);
-    console.log(data);
     res.json(data);
   }
 }
