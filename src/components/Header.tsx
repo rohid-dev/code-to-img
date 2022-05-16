85eb9b056138e7e35738619a4c11f28a2764008c
Rohidul <rohidul209@gmail.com>
2022-05-16 19:55:11 +0600

    bymeacoffee button added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { FaGithub } from "react-icons/fa";
+import Image from "next/image";
+import { FaCoffee, FaGithub } from "react-icons/fa";
 const Header = () => {
   return (
     <div className="w-full py-4 flex items-center px-4 gap-4">
@@ -13,15 +14,25 @@ const Header = () => {
         </a>
       </p>
       <div className="flex-1"></div>
-      {/* <button className="w-10 h-10 flex items-center justify-center hover:bg-white/10 active:bg-white/20 rounded-full">
-        <FaCog className="text-2xl" />
-      </button> */}
       <a
         href="https://github.com/rohidisdev/code-to-img"
         target="_blank"
         className="w-10 h-10 flex items-center justify-center hover:bg-white/10 active:bg-white/20 rounded-full"
       >
         <FaGithub className="text-2xl" />
+      </a>
+      <a
+        href="https://www.buymeacoffee.com/rohid"
+        target="_blank"
+        className="flex items-center"
+      >
+        <Image
+          src="/bmc-button.png"
+          alt="By me a coffee link"
+          height="40px"
+          width="140px"
+          objectFit="contain"
+        />
       </a>
       <ProductHuntBadge />
     </div>
@@ -45,11 +56,14 @@ export const ProductHuntBadge = () => (
   <a
     href="https://www.producthunt.com/posts/code-to-image-converter?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-code&#0045;to&#0045;image&#0045;converter"
     target="_blank"
+    className="flex items-center"
   >
     <img
       src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=346242&theme=light&period=daily"
       alt="Code&#0032;to&#0032;Image&#0032;Converter - Convert&#0032;your&#0032;code&#0032;snippets&#0032;to&#0032;beautiful&#0032;images | Product Hunt"
-      className="h-12"
+      height="40px"
+      width="190px"
+      className="object-contain"
     />
   </a>
 );
